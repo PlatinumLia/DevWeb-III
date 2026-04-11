b03f2f3e65ed8e3e1d6d501168f92c52cca270f1
Lia <134401063+PlatinumLia@users.noreply.github.com>
2026-04-11 16:01:49 -0300

    Add files via upload

---
+++ ListaExercícios_1/exercicio_um/src/main/java/com/example/exercicio_um/ExercicioUmApplication.java
@@ -0,0 +1,12 @@
+package com.example.exercicio_um;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioUmApplication.class, args);
+	}
+}
---
+++ ListaExercícios_1/exercicio_um/src/test/java/com/example/exercicio_um/ExercicioUmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercicio_um;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioUmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
